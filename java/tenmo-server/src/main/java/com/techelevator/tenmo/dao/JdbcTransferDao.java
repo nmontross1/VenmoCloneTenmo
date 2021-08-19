9f6bcc947e2e91825cad05112a66fed160e1beb7
Elton Eze <eltonceze@gmail.com>
2021-08-19 08:57:16 -0400

    fix jdbc query

--- java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ java/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -34,10 +34,9 @@ public class JdbcTransferDao implements TransferDao{
     //#TODO FINSIH IMPLEMENTATION DID NOT TEST METHOD YET.
     @Override
     public Transfer createTransfer(Transfer transfer) {
-        String query = "insert into transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount) " +
-                "values(?,?,?,?,?) RETURNING transfer_id";
+        String query = "insert into transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount) values(?,?,?,?,?) RETURNING transfer_id;";
 
-        long id = jdbcTemplate.update(query,Long.class, transfer.getTransfer_type_id(),transfer.getTransfer_status_id(),transfer.getAccount_from(),transfer.getAccount_to(),transfer.getAmount());
+        Long id = jdbcTemplate.queryForObject(query,Long.class, transfer.getTransfer_type_id(),transfer.getTransfer_status_id(),transfer.getAccount_from(),transfer.getAccount_to(),transfer.getAmount());
         return getTransfer(id);
     }
 
