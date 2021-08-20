bbc5bdf68e4e09b5bcf709651dd8f41895978611
Elton Eze <eltonceze@gmail.com>
2021-08-20 18:44:12 -0400

    Completed extra credit

--- java/tenmo-server/src/test/java/com/techelevator/TenmoDaoTest.java
+++ java/tenmo-server/src/test/java/com/techelevator/TenmoDaoTest.java
@@ -19,15 +19,12 @@ public class TenmoDaoTest {
         dataSource.setUrl("jdbc:postgresql://localhost:5432/tenmo");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres1");
-        /* The following line disables autocommit for connections
-         * returned by this DataSource. This allows us to rollback
-         * any changes after each test */
+
         dataSource.setAutoCommit(false);
     }
     @Before
     public void loadTestData() throws IOException, SQLException {
-        // Spring provides a convenience class called ScriptUtils for running external SQL scripts.
-        // You'll find the test-data.sql script file in the test/resources folder.
+
         ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("tenmo-test-data.sql"));
     }
 
